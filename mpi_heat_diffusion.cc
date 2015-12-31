1c114bbc79241cd0d794c4a499820baa0b8c06c6
Matthew Grasinger <grasingerm@gmail.com>
2015-12-31 00:17:10 -0500

    debugged memory allocation issue with mpi heat diffusion

--- mpi_heat_diffusion.cc
+++ mpi_heat_diffusion.cc
@@ -10,7 +10,7 @@
 static const auto NX        = std::size_t{ 100 };
 static const auto left_val  = float{ 1.0 };
 static const auto right_val = float{ 10.0 };
-static const auto num_steps = unsigned{ 100 };
+static const auto num_steps = unsigned{ 10000 };
 
 void initialize(double uk[], double ukp1[], const std::size_t num_points,
                 const std::size_t num_procs, const int my_id) {
@@ -56,8 +56,8 @@ int main(int argc, char *argv[]) {
 
   printf("id = %d, n = %d\n", myid, num_points); 
 
-  uk    = (double *) malloc(sizeof(double) * num_points + 2);
-  ukp1  = (double *) malloc(sizeof(double) * num_points + 2);
+  uk    = (double *) malloc(sizeof(double) * (num_points + 2));
+  ukp1  = (double *) malloc(sizeof(double) * (num_points + 2));
 
   initialize(uk, ukp1, num_points, num_procs, myid);
 
@@ -67,28 +67,20 @@ int main(int argc, char *argv[]) {
 
 
     if (myid != 0) {
-      MPI_Send(&uk[1], 1, MPI_DOUBLE, lnbr, 2*myid, MPI_COMM_WORLD);
-      cout << "Sending message from " << myid << " to " << lnbr << " with tag "
-           << 2*myid << '\n';
+      MPI_Send(&uk[1], 1, MPI_DOUBLE, lnbr, 1, MPI_COMM_WORLD);
     }
 
     if (myid != num_procs-1) {
-      MPI_Send(&uk[num_points], 1, MPI_DOUBLE, rnbr, 2*myid+1, MPI_COMM_WORLD);
-      cout << "Sending message from " << myid << " to " << rnbr << " with tag "
-           << 2*myid+1 << '\n';
+      MPI_Send(&uk[num_points], 1, MPI_DOUBLE, rnbr, 2, MPI_COMM_WORLD);
     }
 
     if (myid != 0) {
-      MPI_Recv(&uk[0], 1, MPI_DOUBLE, lnbr, 2*myid-1, MPI_COMM_WORLD, &status);
-      cout << "Recieving message at " << myid << " from " << lnbr << " with tag "
-           << 2*myid-1 << '\n';
+      MPI_Recv(&uk[0], 1, MPI_DOUBLE, lnbr, 2, MPI_COMM_WORLD, &status);
     }
 
     if (myid != num_procs-1) {
-      MPI_Recv(&uk[num_points+1], 1, MPI_DOUBLE, rnbr, 2*myid+2, MPI_COMM_WORLD, 
+      MPI_Recv(&uk[num_points+1], 1, MPI_DOUBLE, rnbr, 1, MPI_COMM_WORLD, 
                &status);
-      cout << "Recieving message at " << myid << " from " << rnbr << " with tag "
-           << 2*myid+2 << '\n';
     }
 
     for (int i = 2; i < num_points; ++i)
@@ -115,7 +107,7 @@ int main(int argc, char *argv[]) {
   free(uk);
   free(ukp1);
 
-  MPI_Finalize();
+  cout << MPI_Finalize() << '\n';
 
   return 0;
 }
