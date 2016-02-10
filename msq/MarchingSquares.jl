9f04cb7fe1dfb3143d322c123f19bddbd307c6e9
Matthew Grasinger <grasingerm@gmail.com>
2016-02-10 13:51:38 -0500

    fixing syntax errors and made types more precise for marching squares

--- msq/MarchingSquares.jl
+++ msq/MarchingSquares.jl
@@ -1,30 +1,32 @@
 module MarchingSquares
 
+export @bin_img, bit_cells, disambig_saddle_pts;
+
 #! Binary image
-macro bin_img(data, iso_value)
-  return :(map(x -> (x < $iso_value) ? 0 : 1, $data));
+macro bin_img(gdata, iso_value)
+  return :(map(x -> (x < $iso_value) ? 0::UInt8 : 1::UInt8, $gdata));
 end
 
 #! Representation of contour by bits
 #!
 #! \param   bin_img   Binary image representation
 #! \return            Bit respresentation
-function bit_cells(bin_img::Matrix{Int})
+function bit_cells(bin_img::Matrix{UInt8})
   const nrows   =   size(bin_img, 1) - 1;
   const ncols   =   size(bin_img, 2) - 1;
-  cell_values   =   ones(Int, (nrows, ncols));
+  cell_values   =   zeros(UInt8, (nrows, ncols));
   
   for j=1:ncols, i=1:nrows
-    cells[i, j]  |=    bin_img[i, j];
-    cells[i, j]  <<=   1;
+    cell_values[i, j]  |=    bin_img[i, j];
+    cell_values[i, j]  <<=   1;
 
-    cells[i, j]  |=    bin_img[i, j+1];
-    cells[i, j]  <<=   1;
+    cell_values[i, j]  |=    bin_img[i, j+1];
+    cell_values[i, j]  <<=   1;
 
-    cells[i, j]  |=    bin_img[i+1, j+1];
-    cells[i, j]  <<=   1;
+    cell_values[i, j]  |=    bin_img[i+1, j+1];
+    cell_values[i, j]  <<=   1;
 
-    cells[i, j]  |=    bin_img[i+1, j];
+    cell_values[i, j]  |=    bin_img[i+1, j];
   end
   
   return cell_values;
@@ -35,17 +37,17 @@ end
 #! \param   cell_values   Bit representation of the contour
 #! \param   data          Original data set
 #! \param   iso_value     Iso value
-function disambig_saddle_pts!(cell_values::Matrix{Int}, data::Matrix,
+function disambig_saddle_pts!(cell_values::Matrix{UInt8}, data::Matrix,
                               iso_value::Real)
 
   const nrows, ncols  =   size(cell_values);
 
   for j=1:ncols, i=1:nrows
-    if cell_values[i, j] == 5
+    if cell_values[i, j] == 5::UInt8
       if mean(data[i:i+1, j:j+1]) < iso_value
         cell_values[i, j] <<= 1;
       end
-    elseif cell_values[i, j] == 5
+    elseif cell_values[i, j] == 10::UInt8
       if mean(data[i:i+1, j:j+1]) > iso_value
         cell_values[i, j] >>= 1;
       end
@@ -53,4 +55,4 @@ function disambig_saddle_pts!(cell_values::Matrix{Int}, data::Matrix,
   end
 end
 
-end # m dule
+end # module
