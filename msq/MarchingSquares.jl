191222dafebb2e9200a5963a7f2f24df7334611d
Matthew Grasinger <grasingerm@gmail.com>
2016-02-11 00:11:19 -0500

    fixed some marching squares errors

--- msq/MarchingSquares.jl
+++ msq/MarchingSquares.jl
@@ -1,30 +1,33 @@
 module MarchingSquares
 
+export bin_img, bit_cells, disambig_saddle_pts!;
+
 #! Binary image
-macro bin_img(data, iso_value)
-  return :(map(x -> (x < $iso_value) ? 0 : 1, $data));
+function bin_img(gdata::Matrix, iso_value::Real)
+  f = (x) -> begin; y::UInt8 = (x < iso_value) ? 0 : 1; y; end;
+  return map(f, gdata);
 end
 
 #! Representation of contour by bits
 #!
 #! \param   bin_img   Binary image representation
 #! \return            Bit respresentation
-function bit_cells(bin_img::Matrix{Int})
+function bit_cells(bin_img::Matrix{UInt8})
   const nrows   =   size(bin_img, 1) - 1;
   const ncols   =   size(bin_img, 2) - 1;
-  cell_values   =   ones(Int, (nrows, ncols));
+  cell_values   =   zeros(UInt8, (nrows, ncols));
   
   for j=1:ncols, i=1:nrows
-    cells[i, j]  |=    bin_img[i, j];
-    cells[i, j]  <<=   1;
+    cell_values[i, j]  |=    bin_img[i, j];
+    cell_values[i, j]  <<=   1;
 
-    cells[i, j]  |=    bin_img[i, j+1];
-    cells[i, j]  <<=   1;
+    cell_values[i, j]  |=    bin_img[i, j+1];
+    cell_values[i, j]  <<=   1;
 
-    cells[i, j]  |=    bin_img[i+1, j+1];
-    cells[i, j]  <<=   1;
+    cell_values[i, j]  |=    bin_img[i+1, j+1];
+    cell_values[i, j]  <<=   1;
 
-    cells[i, j]  |=    bin_img[i+1, j];
+    cell_values[i, j]  |=    bin_img[i+1, j];
   end
   
   return cell_values;
@@ -35,7 +38,7 @@ end
 #! \param   cell_values   Bit representation of the contour
 #! \param   data          Original data set
 #! \param   iso_value     Iso value
-function disambig_saddle_pts!(cell_values::Matrix{Int}, data::Matrix,
+function disambig_saddle_pts!(cell_values::Matrix{UInt8}, data::Matrix,
                               iso_value::Real)
 
   const nrows, ncols  =   size(cell_values);
