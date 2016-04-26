badfcdc2c8ed6931b71e90da022982768d7307dd
Matthew Grasinger <grasingerm@gmail.com>
2016-04-26 17:58:27 -0400

    debugged merge sort implementation

--- merge.cc
+++ merge.cc
@@ -1,8 +1,11 @@
 #include <cmath>
+#include <cstring>
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
-template <typename T> void merge(const T* fst, const unsigned n1, const T* snd, 
-                                 const unsigned n2, T* sorted) {
+template <typename T> void _merge(const T* fst, const unsigned n1, const T* snd, 
+                                  const unsigned n2, T* sorted) {
   unsigned i1 = 0, i2 = 0, i3 = 0;
   while (i1 < n1 || i2 < n2) {
     if (i2 >= n2)
@@ -14,29 +17,22 @@ template <typename T> void merge(const T* fst, const unsigned n1, const T* snd,
   }
 }
 
-template <typename T> void mergesort(const T* to_sort, T* sorted, 
-                                     const unsigned n) {
+template <typename T> void _mergesort(T* to_sort, T* warr, const unsigned n) {
   if (n == 1) {
-    sorted[0] = to_sort[0];
     return;
-  }
-  else if (n == 2)
-  {
-    if (to_sort[0] < to_sort[1]) {
-      sorted[0] = to_sort[0];
-      sorted[1] = to_sort[1];
-    }
-    else {
-      sorted[0] = to_sort[1];
-      sorted[1] = to_sort[0];
-    }
   }
   else {
     const unsigned mid = n / 2;
-    mergesort(to_sort, sorted, mid);
-    mergesort(to_sort+mid, sorted+mid, n-mid);
-    merge(to_sort, mid, to_sort+mid, n-mid, sorted);
+    _mergesort(to_sort, warr, mid);
+    _mergesort(to_sort+mid, warr+mid, n-mid);
+    _merge(to_sort, mid, to_sort+mid, n-mid, warr);
+    std::copy(warr, warr+n, to_sort);
   }
+}
+
+template <typename T> void mergesort(T* to_sort, const unsigned n) {
+  std::vector<T> temp_memory(n);
+  _mergesort(to_sort, temp_memory.data(), n);
 }
 
 template <typename T> void print_array(const T* arr, const unsigned n) {
@@ -47,45 +43,47 @@ template <typename T> void print_array(const T* arr, const unsigned n) {
 int main() {
   const static int n1 = 2;
   int unsorted1[n1] = {5, 1};
-  int sorted1[n1] = {0};
 
-  std::cout << "First pair of arrays before mergesort\n";
+  std::cout << "First array before mergesort\n";
   print_array(unsorted1, n1);
-  print_array(sorted1, n1);
   
-  mergesort(unsorted1, sorted1, n1);
+  mergesort(unsorted1, n1);
 
-  std::cout << "First pair of arrays after mergesort\n";
+  std::cout << "First array after mergesort\n";
   print_array(unsorted1, n1);
-  print_array(sorted1, n1);
 
   const static int n2 = 6;
   int unsorted2[n2] = {3, 5, 1, 7, -4, 4};
-  int sorted2[n2] = {0};
 
-  std::cout << "Second pair of arrays before mergesort\n";
+  std::cout << "Second array before mergesort\n";
   print_array(unsorted2, n2);
-  print_array(sorted2, n2);
   
-  mergesort(unsorted2, sorted2, n2);
+  mergesort(unsorted2, n2);
 
-  std::cout << "Second pair of arrays after mergesort\n";
+  std::cout << "Second array after mergesort\n";
   print_array(unsorted2, n2);
-  print_array(sorted2, n2);
 
   const static int n3 = 3;
   int unsorted3[n3] = {1, 7, -4};
-  int sorted3[n3] = {0};
 
-  std::cout << "Third pair of arrays before mergesort\n";
+  std::cout << "Third array before mergesort\n";
   print_array(unsorted3, n3);
-  print_array(sorted3, n3);
   
-  mergesort(unsorted3, sorted3, n3);
+  mergesort(unsorted3, n3);
 
-  std::cout << "Third pair of arrays after mergesort\n";
+  std::cout << "Third array after mergesort\n";
   print_array(unsorted3, n3);
-  print_array(sorted3, n3);
+
+  std::vector<double> unsorted4 {6.9, 3.3, -4.9, 2.2, 1.1, 19.3, -14.0, 2.32, 
+                                 6.0, 9.2};
+
+  std::cout << "Fourth array before mergesort\n";
+  print_array(unsorted4.data(), unsorted4.size());
+  
+  mergesort(unsorted4.data(), unsorted4.size());
+
+  std::cout << "Fourth array after mergesort\n";
+  print_array(unsorted4.data(), unsorted4.size());
 
   return 0;
 }
