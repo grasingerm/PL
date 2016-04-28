0bb25eaa66b1944c5e083019b56408e03522780c
Matthew Grasinger <grasingerm@gmail.com>
2016-04-28 13:23:09 -0400

    modified mergesort to use threading, it ended up being MUCH slower

---
+++ pmerge.cc
@@ -0,0 +1,111 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <iterator>
+#include <functional>
+#include <vector>
+#include <thread>
+#include "ballin_prof.hh"
+
+template <typename T> void _merge(const T* fst, const unsigned n1, const T* snd, 
+                                  const unsigned n2, T* sorted) {
+  unsigned i1 = 0, i2 = 0, i3 = 0;
+  while (i1 < n1 || i2 < n2) {
+    if (i2 >= n2)
+      sorted[i3++] = fst[i1++]; 
+    else if (i1 >= n1 || snd[i2] < fst[i1])
+      sorted[i3++] = snd[i2++]; 
+    else
+      sorted[i3++] = fst[i1++]; 
+  }
+}
+
+template <typename T> void _mergesort(T* to_sort, T* warr, const unsigned n) {
+  if (n == 1) {
+    return;
+  }
+  else {
+    const unsigned mid = n / 2;
+    _mergesort(to_sort, warr, mid);
+    _mergesort(to_sort+mid, warr+mid, n-mid);
+    _merge(to_sort, mid, to_sort+mid, n-mid, warr);
+    std::copy(warr, warr+n, to_sort);
+  }
+}
+
+template <typename T> void mergesort(T* to_sort, const unsigned n) {
+  std::vector<T> temp_memory(n);
+  _mergesort<T>(to_sort, temp_memory.data(), n);
+}
+
+template <typename T> void _pmerge(const T* fst, const unsigned n1, const T* snd, 
+                                   const unsigned n2, T* sorted) {
+  unsigned i1 = 0, i2 = 0, i3 = 0;
+  while (i1 < n1 || i2 < n2) {
+    if (i2 >= n2)
+      sorted[i3++] = fst[i1++]; 
+    else if (i1 >= n1 || snd[i2] < fst[i1])
+      sorted[i3++] = snd[i2++]; 
+    else
+      sorted[i3++] = fst[i1++]; 
+  }
+}
+
+template <typename T> void _pmergesort(T* to_sort, T* warr, const unsigned n) {
+  if (n == 1) {
+    return;
+  }
+  else {
+    const unsigned mid = n / 2;
+    std::thread left  (_pmergesort<T>, to_sort, warr, mid);
+    std::thread right (_pmergesort<T>, to_sort+mid, warr+mid, n-mid);
+    left.join();
+    right.join();
+    _pmerge<T>(to_sort, mid, to_sort+mid, n-mid, warr);
+    std::copy(warr, warr+n, to_sort);
+  }
+}
+
+template <typename T> void pmergesort(T* to_sort, const unsigned n) {
+  std::vector<T> temp_memory(n);
+  _pmergesort<T>(to_sort, temp_memory.data(), n);
+}
+
+template <typename T> void print_array(const T* arr, const unsigned n) {
+  for (unsigned i = 0; i < n-1; ++i) std::cout << arr[i] << ", ";
+  std::cout << arr[n-1] << '\n';
+}
+
+using namespace std;
+
+int main() {
+  random_device rnd_device;
+  mt19937 mersenne_engine(rnd_device());
+  uniform_int_distribution<int> dist(-100, 100);
+
+  auto gen = std::bind(dist, mersenne_engine);
+  vector<int> rvec(100);
+  generate(begin(rvec), end(rvec), gen);
+
+  vector<int> svec1(rvec);
+  vector<int> svec2(rvec);
+
+  cout << "Original, random array\n";
+  print_array(rvec.data(), rvec.size());
+
+  cout << "Mergesort without threading...\n";
+  tic();
+  mergesort(svec1.data(), svec1.size());
+  toc();
+  print_array(svec1.data(), svec1.size());
+
+  cout << "Mergesort with threading...\n";
+  tic();
+  pmergesort(svec2.data(), svec2.size());
+  toc();
+  print_array(svec2.data(), svec2.size());
+
+  return 0;
+}
