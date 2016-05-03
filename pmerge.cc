f2915a3336fb209aeda839b587e629d787d8ca80
Matthew Grasinger <grasingerm@gmail.com>
2016-05-02 23:45:11 -0400

    refactored multi-threaded mergesort

--- pmerge.cc
+++ pmerge.cc
@@ -53,24 +53,32 @@ template <typename T> void _pmerge(const T* fst, const unsigned n1, const T* snd
   }
 }
 
-template <typename T> void _pmergesort(T* to_sort, T* warr, const unsigned n) {
+template <typename T> void _pmergesort(T* to_sort, T* warr, const unsigned n,
+                                       const unsigned N) {
   if (n == 1) {
     return;
   }
   else {
     const unsigned mid = n / 2;
-    std::thread left  (_pmergesort<T>, to_sort, warr, mid);
-    std::thread right (_pmergesort<T>, to_sort+mid, warr+mid, n-mid);
-    left.join();
-    right.join();
+    if (N > 0) {
+      std::thread left  (_pmergesort<T>, to_sort, warr, mid, N-2);
+      std::thread right (_pmergesort<T>, to_sort+mid, warr+mid, n-mid, N-2);
+      left.join();
+      right.join();
+    }
+    else {
+      _mergesort<T>(to_sort, warr, mid);
+      _mergesort<T>(to_sort+mid, warr+mid, n-mid);
+    }
     _pmerge<T>(to_sort, mid, to_sort+mid, n-mid, warr);
     std::copy(warr, warr+n, to_sort);
   }
 }
 
-template <typename T> void pmergesort(T* to_sort, const unsigned n) {
+template <typename T> void pmergesort(T* to_sort, const unsigned n,
+                                      const unsigned N = 8) {
   std::vector<T> temp_memory(n);
-  _pmergesort<T>(to_sort, temp_memory.data(), n);
+  _pmergesort<T>(to_sort, temp_memory.data(), n, N);
 }
 
 template <typename T> void print_array(const T* arr, const unsigned n) {
@@ -86,11 +94,12 @@ int main() {
   uniform_int_distribution<int> dist(-100, 100);
 
   auto gen = std::bind(dist, mersenne_engine);
-  vector<int> rvec(100);
+  vector<int> rvec(50000);
   generate(begin(rvec), end(rvec), gen);
 
   vector<int> svec1(rvec);
   vector<int> svec2(rvec);
+  vector<int> svec3(rvec);
 
   cout << "Original, random array\n";
   print_array(rvec.data(), rvec.size());
@@ -99,13 +108,29 @@ int main() {
   tic();
   mergesort(svec1.data(), svec1.size());
   toc();
-  print_array(svec1.data(), svec1.size());
+  print_array(svec1.data(), 15);
+  std::cout << "...\n";
 
-  cout << "Mergesort with threading...\n";
+  cout << "Mergesort with 8 threads...\n";
   tic();
   pmergesort(svec2.data(), svec2.size());
   toc();
-  print_array(svec2.data(), svec2.size());
+  print_array(svec2.data(), 15);
+  std::cout << "...\n";
+
+  cout << "Mergesort with 4 threads...\n";
+  tic();
+  pmergesort(svec3.data(), svec3.size(), 4);
+  toc();
+  print_array(svec3.data(), 15);
+  std::cout << "...\n";
+
+  cout << "Mergesort with 2 threads...\n";
+  tic();
+  pmergesort(svec3.data(), svec3.size(), 2);
+  toc();
+  print_array(svec3.data(), 15);
+  std::cout << "...\n";
 
   return 0;
 }
