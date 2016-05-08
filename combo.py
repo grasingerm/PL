ae110b7c8d97d56c3568c5b3e9121072fb344353
Matthew Grasinger <grasingerm@gmail.com>
2016-05-08 15:06:18 -0400

    more recursive combo functions in python

--- combo.py
+++ combo.py
@@ -6,3 +6,13 @@ def c(*args):
              c(e, *args[:-1])
              
 c([1, 2], [3, 4], [5, 6])
+
+def cdict(s, *args):
+     if type(args[-1]) != dict:
+         print(s[:-1], *args)
+     else:
+         for (k, v) in args[-1].items():
+             c(k + "_" + s, v, *args[:-1])
+
+cdict("", {"alpha" : 3, "beta": 4}, {"": "poop"}, {"apple": 2, "banana": 89})
+
