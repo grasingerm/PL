c5c3937b6b29da847b8eec96404d9a6a89a2eb0c
Matthew Grasinger <grasingerm@gmail.com>
2016-06-22 16:33:12 -0400

    game edits

--- game.py
+++ game.py
@@ -1,5 +1,52 @@
 from tkinter import *
 import random
+import math
+
+class general(object):
+    def __init__(self,x,y,width,height,otype):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.type = otype
+
+    def setpos(self,x,y):
+        self.x=x
+        self.y=y
+
+    def display(self,canvas):
+        if (self.type=="cloud"):
+            canvas.create_oval(self.x, self.y, self.x+self.width, self.y+self.height, fill="blue")
+        elif (self.type=="wall"):
+            canvas.create_rectangle(self.x-self.width/2,self.y-self.height/2,self.x+self.width/2,self.y+self.height/2,fill="brown")
+        elif (self.type=="pipe"):
+            canvas.create_oval(self.x-20,self.y-20, self.x+20,self.y+20,fill="cyan")
+        elif (self.type == "flag"):
+            canvas.create_rectangle(self.x,self.y,self.x+self.width,self.y+self.height,fill="black")
+        else:
+            canvas.create_rectangle(self.x-20,self.y-20,self.x+20,self.y+20, fill="orange") # error checking
+
+def createCloud(data):
+    y = random.randint(70,150)
+    x = random.randint(30,2000)
+    width = random.randint(25,40)
+    height = random.randint(15,20)
+    for i in range(len(data.w)):
+        if isIntersect(data.c[i].x,data.c[i].y,x,y,data.c[i].width/2 + width/2,data.c[i].height/2 + height/2):
+            createCloud(data)
+            return False
+    data.c.append(general(x,y,width,height,"cloud"))
+
+def createWall(data):
+    y = random.randint(150,290)
+    x = random.randint(50,500)
+    width = random.randint(15,25)
+    height = random.randint(15,30)
+    for i in range(len(data.w)):
+        if isIntersect(data.w[i].x,data.w[i].y,x,y,data.w[i].width/2 + width/2,data.w[i].height/2 + height/2):
+            createWall(data)
+            return False
+    data.w.append(general(x,y,width,height,"wall"))
 
 def init(data):
     data.scrollX = 0  # amount view is scrolled to the right
@@ -16,6 +63,8 @@ def init(data):
     data.wallHeight = [20,12,30,20,30]
     data.currentWallHit = -1 # start out not hitting a wall
     data.clouds = 10
+    data.c = [] # clouds
+    data.w = [] # walls
     data.cloudColor = "blue"
     data.cloudWidth = 50
     data.cloudHeight = 20
@@ -23,13 +72,21 @@ def init(data):
     data.cloudOffsets = []
     data.wallPoints = [0]*data.walls
     data.lineHeight = 10
-
     for i in range(data.clouds):
-        offset_x = random.uniform(-10, 10)
-        offset_y = random.uniform(-10, 10)
-        data.cloudOffsets.append((offset_x, offset_y))
+        createCloud(data)
+    for i in range(data.walls):
+        createWall(data)
+    data.flag = general(200,100,5,200,"flag")
+
     data.groundY = data.height
-    data.gravityMag = 2
+    data.gravityMag = 3
+    data.pipeImg = PhotoImage(file="pipe.png")
+    data.p = [general(100, 300, 50, 100, "pipe")]
+
+def isIntersect(s1x,s1y,s2x,s2y,dx,dy):
+    if abs(s1x-s2x)<dx and abs(s1y-s2y)<dy:
+        return True
+    return False
 
 def getPlayerBounds(data):
     # returns absolute bounds, not taking scrollX into account
@@ -37,10 +94,10 @@ def getPlayerBounds(data):
     (x1, y0) = (x0 + data.playerWidth, y1 - data.playerHeight)
     return (x0, y0, x1, y1)
 
-def getWallBounds(wall, data):
+def getBounds(obj):
     # returns absolute bounds, not taking scrollX into account
-    (x0, y1) = (data.wallX[wall], data.wallY[wall])
-    (x1, y0) = (x0 + data.wallWidth[wall], y1 - data.wallHeight[wall])
+    (x0, y1) = (obj.x-obj.width/2, obj.y+obj.height/2)
+    (x1, y0) = (x0 + obj.width, y1 - obj.height)
     return (x0, y0, x1, y1)
 
 def getCloudBounds(cloud, data):
@@ -52,17 +109,6 @@ def getCloudBounds(cloud, data):
                 y1 - data.cloudHeight + offset_y)
     return (x0, y0, x1, y1)
 
-def getWallHit(data):
-    # return wall that player is currently hitting
-    # note: this should be optimized to only check the walls that are visible
-    # or even just directly compute the wall without a loop
-    playerBounds = getPlayerBounds(data)
-    for wall in range(data.walls):
-        wallBounds = getWallBounds(wall, data)
-        if (boundsIntersect(playerBounds, wallBounds) == True):
-            return wall
-    return -1
-
 def boundsIntersect(boundsA, boundsB):
     # return l2<=r1 and t2<=b1 and l1<=r2 and t1<=b2
     (ax0, ay0, ax1, ay1) = boundsA
@@ -73,24 +119,38 @@ def boundsIntersect(boundsA, boundsB):
 def movePlayer(dx, dy, data):
     data.playerX += dx
     data.playerY += dy
-    # scroll to make player visible as needed
     if (data.playerX < data.scrollX + data.scrollMargin):
         data.scrollX = data.playerX - data.scrollMargin
     if (data.playerX > data.scrollX + data.width - data.scrollMargin):
         data.scrollX = data.playerX - data.width + data.scrollMargin
-    # and check for a new wall hit
-    wall = getWallHit(data)
-    if (wall != data.currentWallHit):
-        data.currentWallHit = wall
-        if (wall >= 0):
-            data.wallPoints[wall] += 1
-    if (wall != -1):
+
+    pbounds = getPlayerBounds(data)
+    hit = False
+    for i in range(len(data.w)):
+        if boundsIntersect(pbounds, getBounds(data.w[i])):
+            hit = True
+    if hit:
         data.playerX -= dx
         data.playerY -= dy
+
+    # scroll to make player visible as needed
+    # if (data.playerX < data.scrollX + data.scrollMargin):
+    #     data.scrollX = data.playerX - data.scrollMargin
+    # if (data.playerX > data.scrollX + data.width - data.scrollMargin):
+    #     data.scrollX = data.playerX - data.width + data.scrollMargin
+    # and check for a new wall hit
+    # wall = getWallHit(data)
+    # if (wall != data.currentWallHit):
+    #     data.currentWallHit = wall
+    #     if (wall >= 0):
+    #         data.wallPoints[wall] += 1
+    # if (wall != -1):
+    #     data.playerX -= dx
+    #     data.playerY -= dy
     if not data.playerY > (data.height - data.groundY + data.lineHeight):
         data.playerY = data.height - data.groundY + data.lineHeight
-    if data.playerX > data.mapWidth:
-        data.playerX = 0
+    # if data.playerX > data.mapWidth:
+    #     data.playerX = 0
 
 def gravity(data):
     if data.playerY > (data.height - data.groundY):
@@ -115,24 +175,29 @@ def redrawAll(canvas, data):
     # draw the walls
     # (Note: should optimize to only consider walls that can be visible now!)
     sx = data.scrollX
-    for wall in range(data.walls):
-        (x0, y0, x1, y1) = getWallBounds(wall, data)
-        fill = "orange" if (wall == data.currentWallHit) else "pink"
-        canvas.create_rectangle(x0-sx, y0, x1-sx, y1, fill=fill)
-        (cx, cy) = ((x0+x1)/2 - sx, (y0 + y1)/2)
-        canvas.create_text(cx, cy, text=str(data.wallPoints[wall]))
-        cy = lineY + 5
-        canvas.create_text(cx, cy, text=str(wall), anchor=N)
+
+    for i in range(len(data.w)):
+        data.w[i].x -= sx
+        data.w[i].display(canvas)
+        data.w[i].x += sx
 
     # draw clouds
-    for wall in range(data.clouds):
-        (x0, y0, x1, y1) = getCloudBounds(wall, data)
-        canvas.create_oval(x0-sx, y0, x1-sx, y1, fill=data.cloudColor)
+    for i in range(len(data.c)):
+        data.c[i].x -= sx
+        data.c[i].display(canvas)
+        data.c[i].x += sx
+
+    # draw flag
+    data.flag.x -= sx
+    data.flag.display(canvas)
+    data.flag.y += sx
 
     # draw the player
     (x0, y0, x1, y1) = getPlayerBounds(data)
     canvas.create_oval(x0 - sx, y0, x1 - sx, y1, fill="cyan")
-
+   
+    for p in data.p:
+        canvas.create_image(p.x - sx, p.y, image=data.pipeImg)
 
 ####################################
 # use the run function as-is
@@ -163,11 +228,11 @@ def run(width=300, height=300):
     data.width = width
     data.height = height
     data.timerDelay = 100 # milliseconds
-    init(data)
     # create the root and the canvas
     root = Tk()
     canvas = Canvas(root, width=data.width, height=data.height)
     canvas.pack()
+    init(data)
     # set up events
     root.bind("<Button-1>", lambda event:
                             mousePressedWrapper(event, canvas, data))
