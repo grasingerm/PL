8a366e0b0ba79ed3c99e65ffb565cb772394fc7f
Matthew Grasinger <grasingerm@gmail.com>
2016-08-26 11:41:16 -0400

    cpp example with unicode variable names

---
+++ unicode_vars.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double σ = 1.3;
+  double ε = 2.2;
+
+  cout << σ << '\n';
+  cout << ε << '\n';
+  cout << "σ * ε = " << σ * ε << '\n';
+
+  return 0;
+}
