32f96053655a785e13f8519f3e548ff4ce1ad302
Matthew Grasinger <grasingerm@gmail.com>
2016-09-22 16:15:22 -0400

    example with global variable defined in a source file

---
+++ extern/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "x.h"
+
+int main() {
+  printf("Hello world! x = %d\n", x);
+  return 0;
+}
---
+++ extern/x.h
@@ -0,0 +1,6 @@
+#ifndef __X_H__
+#define __X_H__
+
+extern const int x;
+
+#endif
