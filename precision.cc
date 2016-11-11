50f565a1d66cb6f6d0631f83d5ba9477d0b5a957
Matthew Grasinger <grasingerm@gmail.com>
2016-11-11 11:32:32 -0500

    mpi example with io

--- precision.cc
+++ precision.cc
@@ -6,10 +6,23 @@ using namespace std;
 
 int main()
 {
+  cout << "Double\n";
   cout << setw(20) << "Max precision: " << numeric_limits<double>::epsilon() << "\n";
   cout << setw(20) << "Min value: " << numeric_limits<double>::min() << "\n";
   cout << setw(20) << "Max value: " << numeric_limits<double>::max() << "\n";
   cout << setw(20) << "Max round error: " << numeric_limits<double>::round_error() << "\n";
+
+  cout << "Long double\n";
+  cout << setw(20) << "Max precision: " << numeric_limits<long double>::epsilon() << "\n";
+  cout << setw(20) << "Min value: " << numeric_limits<long double>::min() << "\n";
+  cout << setw(20) << "Max value: " << numeric_limits<long double>::max() << "\n";
+  cout << setw(20) << "Max round error: " << numeric_limits<long double>::round_error() << "\n";
+
+  cout << "Long unsigned\n";
+  cout << setw(20) << "Max precision: " << numeric_limits<long unsigned>::epsilon() << "\n";
+  cout << setw(20) << "Min value: " << numeric_limits<long unsigned>::min() << "\n";
+  cout << setw(20) << "Max value: " << numeric_limits<long unsigned>::max() << "\n";
+  cout << setw(20) << "Max round error: " << numeric_limits<long unsigned>::round_error() << "\n";
 
   return 0;
 }
