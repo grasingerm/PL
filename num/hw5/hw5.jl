988171752d3f242b2f8f79df7cbb11ef2e887ec5
Matthew Grasinger <grasingerm@gmail.com>
2017-04-18 12:47:17 -0400

    solved issue with problem c1

--- num/hw5/hw5.jl
+++ num/hw5/hw5.jl
@@ -1,28 +1,21 @@
-using PyPlot;
+using Plots;
 
-asoln(x, y, t) = exp(0.75 * t) * sin(2 * x * y) * cosh(1.5 * (x + y));
+asoln(x, y, t) = exp(0.75 * t) * sin(2 * x - y) * cosh(1.5 * (x + y));
 
-const debug_flag = true;
+errs = [];
+hs = [1/10; 1/20; 1/40];
 
-macro if_debug(code)
-  if debug_flag
-    return code;
-  end
-end
-
-for h in [1/10; 1/20; 1/40]
-  w = open("h-$h.csv", "w");
-
-  const μ = 1.0;
-  const k = μ * h*h;
+for h in hs
+  k = h;
+  μ = k / (h*h);
   println("k = $k, h = $h");
-
-  const aax = -k / (2 * h*h);
-  const bbx = (k / (h*h) + 1);
+  
+  const aax = -μ / 2;
+  const bbx = (μ + 1);
   const ccx = aax;
 
-  const aay = -k / (h*h);
-  const bby = (2 * k / (h*h) + 1);
+  const aay = -μ;
+  const bby = (2 * μ + 1);
   const ccy = aay;
 
   xs = linspace(0.0, 1.0, Int(round(1.0 / h)));
@@ -34,38 +27,40 @@ for h in [1/10; 1/20; 1/40]
   for (m, x) in zip(1:M, xs), (l, y) in zip(1:L, ys)
     u[m, l, 1] = asoln(x, y, 0);
   end
-  u_debug = (debug_flag) ? zeros(M, L, K) : zeros(1, 1, 1);
-  @if_debug(for (m, x) in zip(1:M, xs), (l, y) in zip(1:L, ys)
-    u_debug[m, l, 1] = asoln(x, y, 0);
-  end);
  
   for n in 1:K-1
+    u_temp = zeros(M, L);
     # calculate boundary terms
     for l in 1:L
+      u_temp[1, l] = asoln(0.0, ys[l], ts[n+1]);
+      u_temp[M, l] = asoln(1.0, ys[l], ts[n+1]);
       u[1, l, n+1] = asoln(0.0, ys[l], ts[n+1]);
       u[M, l, n+1] = asoln(1.0, ys[l], ts[n+1]);
     end
     for m in 2:M-1
+      u_temp[m, 1] = asoln(xs[m], 0.0, ts[n+1]);
+      u_temp[m, L] = asoln(xs[m], 1.0, ts[n+1]);
       u[m, 1, n+1] = asoln(xs[m], 0.0, ts[n+1]);
       u[m, L, n+1] = asoln(xs[m], 1.0, ts[n+1]);
     end
 
-    for l in 2:L-1 # consider boundary terms
+    for l in 2:L-1
       # calculate pi and qi for Thomas' algorithm
       p = zeros(L);
       q = zeros(L);
       p[2], q[2] = 0.0, asoln(0.0, ys[l], ts[n+1]);
       for m=2:M-1
-        dd = u[m, l, n] + (k / (h*h) * 
+        dd = u[m, l, n] + (μ * 
                            (u[m, l+1, n] - 2 * u[m, l, n] + u[m, l-1, n]));
         denom = aax * p[m] + bbx;
         p[m+1] =  -ccx / denom;
         q[m+1] = (dd - aax * q[m]) / denom;
       end
-      u[M, l, n+1] = asoln(1.0, ys[l], ts[n+1]);
+      u_temp[M, l] = asoln(1.0, ys[l], ts[n+1]);
       for m=M-1:-1:2
-        u[m, l, n+1] = p[m+1] * u[m+1, l, n+1] + q[m+1];
+        u_temp[m, l] = p[m+1] * u_temp[m+1, l] + q[m+1];
       end
+      u_temp[1, l] = asoln(0.0, ys[l], ts[n+1]);
     end
 
     for m in 2:M-1
@@ -74,39 +69,50 @@ for h in [1/10; 1/20; 1/40]
       q = zeros(M);
       p[2], q[2] = 0.0, asoln(xs[m], 0.0, ts[n+1]);
       for l=2:L-1
-        # Note: n+1 is because we need to use values calculated in last sweep
-        dd = u[m, l, n+1] + (k / (2*h*h) * 
-                         (u[m+1, l, n+1] - 2 * u[m, l, n+1] + u[m-1, l, n+1]));
-        denom = aay * p[m] + bby;
-        p[m+1] =  -ccy / denom;
-        q[m+1] = (dd - aay * q[m]) / denom;
+        dd = u_temp[m, l] + (μ / 2 * 
+                         (u_temp[m+1, l] - 2 * u_temp[m, l] + u_temp[m-1, l]));
+        denom = aay * p[l] + bby;
+        p[l+1] =  -ccy / denom;
+        q[l+1] = (dd - aay * q[l]) / denom;
       end
       u[m, L, n+1] = asoln(xs[m], 1.0, ts[n+1]);
       for l=L-1:-1:2
-        u[m, l, n+1] = p[m+1] * u[m, l+1, n+1] + q[m+1];
+        u[m, l, n+1] = p[l+1] * u[m, l+1, n+1] + q[l+1];
       end
+      u[m, 1, n+1] = asoln(xs[m], 0.0, ts[n+1]);
     end
 
-    @if_debug(begin
-        for l in 1:L
-          u_debug[1, l, n+1] = asoln(0.0, ys[l], ts[n+1]);
-          u_debug[M, l, n+1] = asoln(1.0, ys[l], ts[n+1]);
-        end
-        for m in 2:M-1
-          u_debug[m, 1, n+1] = asoln(xs[m], 0.0, ts[n+1]);
-          u_debug[m, L, n+1] = asoln(xs[m], 1.0, ts[n+1]);
-        end
-        for l in 2:L-1, m in 2:M-1
-          u_debug[m, l, n+1] = k / (h*h) * (u_debug[m+1, l, n] - 2 * u_debug[m, l, n] 
-                                            + u_debug[m-1, l, n] + 
-                                            2 * (u_debug[m, l+1, n] - 2 * u_debug[m, l, n] 
-                                                 + u_debug[m, l-1, n])) + u_debug[m, l, n];
-        end
-      end);
-
+    u_exact = zeros(M, L);
     for m in 1:M, l in 1:L
-      write(w, "$(ts[n+1]), $(xs[m]), $(ys[l]), $(u[m, l, n+1]), $(asoln(xs[m], ys[l], ts[n+1])), $(u_debug[m, l, n+1])\n");
+      u_exact[m, l] = asoln(xs[m], ys[l], ts[n+1]);
+    end
+
+    if n % 5 == 0
+      println("t=$(ts[n+1]), relative L∞ error: ", norm(u[:, :, n+1] - u_exact, Inf) / norm(u_exact, Inf));
+      open(w -> begin
+        for m in 1:M, l in 1:L
+          write(w, "$(xs[m]),$(ys[l]),$(u[m, l, n+1]),$(asoln(xs[m], ys[l], ts[n+1]))\n");
+        end
+        end, "h-$(Int(round(h*100)))_t-$(Int(round(ts[n+1]*100))).csv", "w");
     end
   end
-  close(w);
+
+  u_exact = zeros(M, L);
+  for m in 1:M, l in 1:L
+    u_exact[m, l] = asoln(xs[m], ys[l], ts[K]);
+  end
+  push!(errs, maximum(map(x -> abs(x), u[:, :, K] - u_exact)));
+  println("t=1.0, relative L∞ error: ", norm(u[:, :, K] - u_exact, Inf) / norm(u_exact, Inf));
+  println();
+
+  open(w -> begin
+    for m in 1:M, l in 1:L
+      write(w, "$(xs[m]),$(ys[l]),$(u[m, l, K]),$(asoln(xs[m], ys[l], ts[K]))\n");
+    end
+    end, "h-$(Int(round(h*100)))_end.csv", "w");
 end
+
+println(@sprintf("%10s %10s %10s", "h", "max(|e|)", "ratio"));
+println(@sprintf("%10.2lf %10.2lf %10s", hs[1], errs[1], "N/A"));
+println(@sprintf("%10.2lf %10.2lf %10.lf", hs[2], errs[2], errs[1]/errs[2]));
+println(@sprintf("%10.2lf %10.2lf %10.lf", hs[3], errs[3], errs[2]/errs[3]));
