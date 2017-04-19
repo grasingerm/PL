eb839b8e854849bc5cf90444abd271f993ad990a
Matthew Grasinger <grasingerm@gmail.com>
2017-04-19 01:04:43 -0400

    finished hw5

--- num/hw5/hw5.jl
+++ num/hw5/hw5.jl
@@ -1,5 +1,3 @@
-using Plots;
-
 asoln(x, y, t) = exp(0.75 * t) * sin(2 * x - y) * cosh(1.5 * (x + y));
 
 errs = [];
@@ -30,16 +28,17 @@ for h in hs
  
   for n in 1:K-1
     u_temp = zeros(M, L);
+    thalf = (ts[n]+ts[n+1]) / 2;
     # calculate boundary terms
     for l in 1:L
-      u_temp[1, l] = asoln(0.0, ys[l], ts[n+1]);
-      u_temp[M, l] = asoln(1.0, ys[l], ts[n+1]);
+      u_temp[1, l] = asoln(0.0, ys[l], thalf);
+      u_temp[M, l] = asoln(1.0, ys[l], thalf);
       u[1, l, n+1] = asoln(0.0, ys[l], ts[n+1]);
       u[M, l, n+1] = asoln(1.0, ys[l], ts[n+1]);
     end
     for m in 2:M-1
-      u_temp[m, 1] = asoln(xs[m], 0.0, ts[n+1]);
-      u_temp[m, L] = asoln(xs[m], 1.0, ts[n+1]);
+      u_temp[m, 1] = asoln(xs[m], 0.0, thalf);
+      u_temp[m, L] = asoln(xs[m], 1.0, thalf);
       u[m, 1, n+1] = asoln(xs[m], 0.0, ts[n+1]);
       u[m, L, n+1] = asoln(xs[m], 1.0, ts[n+1]);
     end
@@ -48,7 +47,7 @@ for h in hs
       # calculate pi and qi for Thomas' algorithm
       p = zeros(L);
       q = zeros(L);
-      p[2], q[2] = 0.0, asoln(0.0, ys[l], ts[n+1]);
+      p[2], q[2] = 0.0, asoln(0.0, ys[l], thalf);
       for m=2:M-1
         dd = u[m, l, n] + (μ * 
                            (u[m, l+1, n] - 2 * u[m, l, n] + u[m, l-1, n]));
@@ -56,11 +55,11 @@ for h in hs
         p[m+1] =  -ccx / denom;
         q[m+1] = (dd - aax * q[m]) / denom;
       end
-      u_temp[M, l] = asoln(1.0, ys[l], ts[n+1]);
+      u_temp[M, l] = asoln(1.0, ys[l], thalf);
       for m=M-1:-1:2
         u_temp[m, l] = p[m+1] * u_temp[m+1, l] + q[m+1];
       end
-      u_temp[1, l] = asoln(0.0, ys[l], ts[n+1]);
+      u_temp[1, l] = asoln(0.0, ys[l], thalf);
     end
 
     for m in 2:M-1
@@ -89,6 +88,7 @@ for h in hs
 
     if n % 5 == 0
       println("t=$(ts[n+1]), relative L∞ error: ", norm(u[:, :, n+1] - u_exact, Inf) / norm(u_exact, Inf));
+      println("t=$(ts[n+1]), relative L2 error: ", norm(u[:, :, n+1] - u_exact, 2) / norm(u_exact, 2));
       open(w -> begin
         for m in 1:M, l in 1:L
           write(w, "$(xs[m]),$(ys[l]),$(u[m, l, n+1]),$(asoln(xs[m], ys[l], ts[n+1]))\n");
@@ -103,6 +103,7 @@ for h in hs
   end
   push!(errs, maximum(map(x -> abs(x), u[:, :, K] - u_exact)));
   println("t=1.0, relative L∞ error: ", norm(u[:, :, K] - u_exact, Inf) / norm(u_exact, Inf));
+  println("t=1.0, relative L2 error: ", norm(u[:, :, K] - u_exact, 2) / norm(u_exact, 2));
   println();
 
   open(w -> begin
@@ -113,6 +114,6 @@ for h in hs
 end
 
 println(@sprintf("%10s %10s %10s", "h", "max(|e|)", "ratio"));
-println(@sprintf("%10.2lf %10.2lf %10s", hs[1], errs[1], "N/A"));
-println(@sprintf("%10.2lf %10.2lf %10.lf", hs[2], errs[2], errs[1]/errs[2]));
-println(@sprintf("%10.2lf %10.2lf %10.lf", hs[3], errs[3], errs[2]/errs[3]));
+println(@sprintf("%10.4lf %10.4lf %10s", hs[1], errs[1], "N/A"));
+println(@sprintf("%10.4lf %10.4lf %10lf", hs[2], errs[2], errs[1]/errs[2]));
+println(@sprintf("%10.4lf %10.4lf %10lf", hs[3], errs[3], errs[2]/errs[3]));
