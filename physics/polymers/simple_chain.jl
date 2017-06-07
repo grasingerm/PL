74ca446688d56cb4bf9916b6c57ad1a467e4d265
Matthew Grasinger <grasingerm@gmail.com>
2017-06-07 01:51:43 -0400

    still not finished debugging simple chain example

--- physics/polymers/simple_chain.jl
+++ physics/polymers/simple_chain.jl
@@ -40,15 +40,23 @@ s = ArgParseSettings();
   "--dx", "-D"
     help = "element size"
     arg_type = Float64
-    default = 0.2
+    default = 0.1
   "--k1", "-K"
     help = "first spring constant"
     arg_type = Float64
-    default = 0.5
+    default = 1.0
   "--k2", "-L"
     help = "second spring constant"
     arg_type = Float64
-    default = 1.5
+    default = 1.0
+  "--kg", "-G"
+    help = "spring constant for constraint"
+    arg_type = Float64
+    default = 100.0
+  "--r0", "-r"
+  help = "end to end distance"
+    arg_type = Float64
+    default = 1.0
   "--output-step", "-O"
     help = "Frequency with which to output step (% complete)"
     arg_type = Float64
@@ -59,18 +67,57 @@ pa = parse_args(s);
 
 const k1 = pa["k1"];
 const k2 = pa["k2"];
-const K = [k1+k2 -k2; -k2 k2];
+const kg = pa["kg"];
+const r = pa["r0"];
+const beta = pa["beta"];
+const Kexact = [k1+k2 -k2; -k2 k2];
+const K = [k1+k2 -k2; -k2 k2+kg];
+@assert(isposdef(Kexact));
+@assert(isposdef(K));
+const V = eigvecs(K);
+const Lambda = V * K * V';
+@assert(abs(Lambda[1, 2]) < 1e-10);
+@assert(abs(Lambda[2, 1]) < 1e-10);
+const b = V*[0.0; -kg * r];
+Z = (exp(-beta/2 * ((kg * r^2) - b[1]^2 / Lambda[1, 1] - b[2]^2 / Lambda[2, 2])) *
+     sqrt(2 * pi / (beta * Lambda[1, 1])) * sqrt(2 * pi / (beta * Lambda[2, 2])));
 
 const nsteps = pa["num-steps"];
 acc_func = eval(parse(pa["accept"]));
 const delta = pa["delta"];
 const dx = pa["dx"];
 const output_step = Int(round(nsteps * pa["output-step"]));
+const x0 = [0.5*r; r];
+const u0 = potential(x0, Kexact);
+
+x = copy(x0);
+u = u0;
+
+xsum = zeros(2);
+xsqsum = zeros(2);
+usum = 0.0;
+usqsum = 0.0;
+nsamples = 0;
+counter = 0;
 
 for step = 1:nsteps
-  choice = rand(1:5);
   xtrial = copy(x);
-  xtrial[choice] += rand(-delta:1e-9:delta);
+  xtrial[1] += rand(-delta:1e-9:delta);
+  utrial = potential(xtrial, Kexact);
+  
+  if acc_func(u, utrial, beta)
+    x = xtrial;
+    u = utrial;
+  end
+
+  xsum += x;
+  xsqsum += map(y -> y*y, x);
+  usum += u;
+  usqsum += u*u;
+  nsamples += 1;
+  if x[1] >= x0[1]-dx && x[1] <= x0[1]+dx
+    counter += 1;
+  end
   
   if step % output_step == 0
     println(step, " / ", nsteps);
@@ -79,26 +126,20 @@ end
 
 exp_x = xsum / nsamples;
 exp_xsq = xsqsum / nsamples;
-px0 = counter / (nsamples * (2 * dx)^3 * (2 * dtheta)^2);
-Zprop = 1 / px0;
-Z = exp(0) * Zprop;
-exp_E = esum / nsamples;
-exp_E2 = esqsum / nsamples;
+px0 = counter / (nsamples * (2 * dx));
+Z_approx = exp(-beta * u0) / px0;
+exp_E = usum / nsamples;
+exp_E2 = usqsum / nsamples;
 println("delta             =   $delta;");
 println("beta              =   $beta;");
 println("k1                =   $k1;");
 println("k2                =   $k2;");
-println("E0                =   $E0;");
-println("n0                =   $n0;");
-println("p0                =   $p0;");
 println("u0                =   $u0;");
 println("exp_x             =   $(exp_x);");
 println("exp_x2            =   $(exp_xsq);");
 println("Delta_x           =   $(sqrt(exp_xsq - map(x -> x*x, exp_x)));");
-println("exp_pmag          =   $(pmagsum / nsamples);");
 println("exp_E             =   $exp_E;");
 println("exp_E2            =   $exp_E2;");
 println("Delta_E           =   $(sqrt(exp_E2 - exp_E*exp_E));");
-println("exp_E_abs_error   =   $(e_abs_error_sum / nsamples);");
-println("exp_E_sq_error    =   $(e_sq_error_sum / nsamples);");
-println("Z_approx          =   $Z;");
+println("Z_approx          =   $Z_approx;");
+println("Z                 =   $Z;");
